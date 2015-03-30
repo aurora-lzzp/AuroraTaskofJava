041b072177276d3a48b60b0d18644e04668d72b5
筱苏 <@ZXS-Y50>
2015-03-30 23:06:06 +0800

    班级类加入getter setter

--- src/student/ClassInfo.java
+++ src/student/ClassInfo.java
@@ -5,7 +5,27 @@ import java.util.ArrayList;
 public class ClassInfo {
 	private int classnum;
 	private String classname;
-    ArrayList Students = new ArrayList();
+	ArrayList Students = new ArrayList();
+	
+    public int getClassnum() {
+		return classnum;
+	}
+	public void setClassnum(int classnum) {
+		this.classnum = classnum;
+	}
+	public String getClassname() {
+		return classname;
+	}
+	public void setClassname(String classname) {
+		this.classname = classname;
+	}
+	public ArrayList getStudents() {
+		return Students;
+	}
+	public void setStudents(ArrayList students) {
+		Students = students;
+	}
+	
     
     
 }
