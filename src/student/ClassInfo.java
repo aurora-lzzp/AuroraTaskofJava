b860fc46aa6a193de939a2fe3cb448ed2b5d4edb
筱苏 <@ZXS-Y50>
2015-03-30 22:43:52 +0800

    添加了班级类

---
+++ src/student/ClassInfo.java
@@ -0,0 +1,11 @@
+package student;
+
+import java.util.ArrayList;
+
+public class ClassInfo {
+	private int classnum;
+	private String classname;
+    ArrayList Students = new ArrayList();
+    
+    
+}
